ecdbc4532ae9dd38a9288ce4a423c7d5916f8543
Will Wray <wjwray@gmail.com>
2018-11-01 10:32:07 -0400

    Create char_array_extents.cpp
    https://wandbox.org/permlink/5aVvo78zl2w0nYIl
    Not the answer you're looking for, and disqualifies itself for using includes.
    Also, it can't compile for strings as large as "StringProxy" because the resulting char array type is too large
    (even though it is never instantiated)

---
+++ char_array_extents.cpp
@@ -0,0 +1,23 @@
+#include <type_traits>
+#include <utility>
+
+template <typename T> 
+constexpr auto StringProxy()
+{
+  struct SP
+  {
+      char str[std::rank_v<T>];
+      constexpr bool operator==(char const* cp)
+      {
+          for (unsigned i=0; i!=std::rank_v<T> && str[i]==*cp; ++i, ++cp);
+          return !*cp;
+      }
+  };
+  return []<std::size_t... i>(std::index_sequence<i...>){
+    return SP{char(std::extent_v<T,i>)...};
+  }(std::make_index_sequence<std::rank_v<T>>{});
+}
+
+static_assert(StringProxy<char['S']['t']['r']['i']['n']['g']>() == "String", "");
+                                                           
+int main() { return 0; }
