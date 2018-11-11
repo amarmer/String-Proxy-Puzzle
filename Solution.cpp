2d1b323d565ef47ca4ff60dc84c3dab287ba211d
Alex Marmer <amarmer@gmail.com>
2018-11-10 22:03:06 -0800

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,27 @@
+template <typename T>
+constexpr auto StringProxy()
+{
+    struct Aux
+    {
+        constexpr bool operator == (const char* s)
+        {
+            auto compare = [&s](auto compare, auto* pArr)
+            {
+                if constexpr (1 == sizeof(*pArr))
+                    return !*s;
+                else
+                    return (*s++ == sizeof(*pArr)/sizeof(void*)) ? compare(compare, (decltype(**pArr + 0))nullptr) : false;
+            };
+            
+            return compare(compare, (T)nullptr);
+        }
+    };
+    
+    return Aux();
+}
+
+static_assert(StringProxy<char* (*(*(*(*(*(*(*(*(*(*(*)['S'])['t'])['r'])['i'])['n'])['g'])['P'])['r'])['o'])['x'])['y']>() 
+              == "StringProxy");
+static_assert(StringProxy<char*>() == "", "");
+
+int main() { return 0; }
